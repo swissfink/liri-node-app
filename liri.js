ab70f6a4b6fe78169fce51f590b0b50a685f7484
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-03-28 17:07:05 -0400

    3/28 Revision

--- liri.js
+++ liri.js
@@ -1,222 +1,240 @@
-var dotenv = require("dotenv").config();
+require("dotenv").config();
 
 var axios = require("axios");
 
-// var fs = require("fs");
+var Spotify = require('node-spotify-api');
+ 
+var moment = require('moment');
 
-var keys = require("./keys.js");
+var fs = require("fs");
 
-var spotify = new Spotify(keys.spotify);
-
-// var moment = require('moment');
-
-// moment().format();
-
+//////////////////////////////////////////////////
 // First argument
+//////////////////////////////////////////////////
+
 var action = process.argv[2];
 
 switch (action) {
-    case "concert-this":
-      concert();
-      break;
-    
-    case "spotify-this-song":
-      spotify();
-      break;
-    
-    case "movie-this":
-      movie();
-      break;
-    
-    case "do-what-it-says":
-      doit();
-      break;
-    }
 
+  case "concert-this":
+    concertThis();
+    break;
+
+  case "spotify-this-song":
+    spotifyThis();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doitNow();
+    break;
+
+};
+
+//////////////////////////////////////////////////
 //Second Argument
-
-// Concert Search
 //////////////////////////////////////////////////
 
-function concert() {
 
-    // Store all of the arguments in an array
-    var nodeArgs = process.argv;
+///////////////////////////////
+// Concert Search
+///////////////////////////////
 
-    // Create an empty variable for holding the band name
-    var artist = "";
+function concertThis() {
 
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "%20"s as per the documentation
-    for (var i = 3; i < nodeArgs.length; i++) {
+  // Store all of the arguments in an array
+  var nodeArgs = process.argv;
+
+  // Create an empty variable for holding the band name
+  var artistName = "";
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "%20"s as per the documentation
+  for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
-        artist = artist + "%20" + nodeArgs[i];
+      artistName = artistName + "%20" + nodeArgs[i];
     }
     else {
-        artist += nodeArgs[i];
+      artistName += nodeArgs[i];
     }
 
-    console.log(artist);
+    // console.log(artistName);
 
-  }
+  };
 
-    // Run the axios.get function...
-    // The axios.get function takes in a URL and returns a promise (just like $.ajax)
-    // axios
-    // .get("rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    // .then(function(response) {
-    // // If the axios was successful...
-    // // Then log the body from the site!
-    // console.log(response);
-    // })
-    // if (error) {
-    // console.log("\r\n\r\n\r\n");
-    // console.log("Sorry we don't have enough data on that band! Try another one.");
-    // console.log("\r\n\r\n\r\n");
+  // DOCUMENTATION // https://app.swaggerhub.com/apis-docs/Bandsintown/PublicAPI/3.0.0#/artist%20events/artistEvents
 
-    // return;
-    // } else {
+  // Then run a request with axios to the bandsintown API with the artist specified
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
 
-    // console.log("This band will be performing at " + data.venue.name);
-    // console.log("in " + data.venue.city);
-    // console.log("on " + data.datetime);
+  // This line is just to help us debug against the actual URL.
+  // console.log(queryUrl);
 
-    // }
+  axios
+    .get(queryUrl)
+    .then(function (response) {
+      // Response
+      console.log(response.data); // This is working
+      // Name of Venue
+      // console.log("\t This band will be performing at " + response.data.venue); // Undefined
+      // Venue Location
+      // console.log("\t in " + response.data.venue.city); // Not working at all
+      // Date of the Event
+      // console.log("\t on " + moment(response.data.datetime).format("MM DD YYYY")); // Not Working
+  })
 
-}
+  .catch(function (error) {
+    if (error) {
+      console.log(error);
+    }
+  })
+};
 
-
+///////////////////////////////
 // Song Search
-////////////////////////////////////////////////
-function spotify() {
+///////////////////////////////
 
-    // Store all of the arguments in an array
-    var nodeArgs = process.argv;
+function spotifyThis() {
 
-    // Create an empty variable for holding the band name
-    var search = "";
+  // Store all of the arguments in an array
+  var nodeArgs = process.argv;
 
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "%20"s as per the documentation
-    for (var i = 3; i < nodeArgs.length; i++) {
+  // Create an empty variable for holding the band name
+  var songName = "";
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s as per the documentation
+  for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
-        search = search + "%20" + nodeArgs[i];
+      songName = songName + "+" + nodeArgs[i];
     }
     else {
-        search += nodeArgs[i];
+      songName += nodeArgs[i];
     }
 
-    console.log(search);
+    // console.log(songName);
 
   }
 
-  // This call isn't correct, but is needs to be something lek this.
+  var keys = require("./keys.js");
 
-    // // Then run a request with axios to the OMDB API with the movie specified
-    // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  var spotify = new Spotify(keys.spotify);
 
-    // // This line is just to help us debug against the actual URL.
-    // console.log(queryUrl);
+  // DOCUMENTATION // https://developer.spotify.com/documentation/web-api/reference/object-model/#track-object-full 
 
-    // axios.get(queryUrl).then(
-    // function(response)
+  // Then run a request with the Spotify API with the song name specified
+  spotify
+    .search({ type: 'track', query: songName, limit: 10 })
+    .then(function(response) {
+      // Response
+      // console.log(response);
+      // Artist(s)
+      console.log("\t Artist Name: " + response.artists);
+      // Song name
+      console.log("\t Song Title: " + response.name);
+      // A preview link of the song from Spotify
+      console.log("\t Preview Link: " + response.preview_url);
+      // Album name that the song is from
+      console.log("\t Album: " + response.album);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+};
 
-  // Artist(s)
-  console.log("Artist Name: "+ response.data.artists);
-  // Song name
-  console.log("Song Title: "+ response.data.name);
-  // A preview link of the song from Spotify
-  console.log("Preview Link: "+ response.data.preview
-  -url);
-  // Album name that the song is from
-  console.log("Album: "+ response.data.album);
-
-}
-
+///////////////////////////////
 // Movie Search
-////////////////////////////////////////////////
+///////////////////////////////
 
-function movie() {
+function movieThis() {
 
-    // Grab or assemble the movie name and store it in a variable called "movieName"
-    var nodeArgs = process.argv;
+  // Grab or assemble the movie name and store it in a variable called "movieName"
+  var nodeArgs = process.argv;
 
-    // Create an empty variable for holding the movie name in case there are multiple words in the movie title.
-    var movieName = "";
+  // Create an empty variable for holding the movie name in case there are multiple words in the movie title.
+  var movieName = "";
 
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "+"s
-    for (var i = 3; i < nodeArgs.length; i++) {
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
+      movieName = movieName + "+" + nodeArgs[i];
     }
 
     else {
-        movieName += nodeArgs[i];
+      movieName += nodeArgs[i];
     }
 
     // console.log(movieName);
 
-  }
+  };
+
+  // DOCUMENTATION // https://media.readthedocs.org/pdf/omdbpy/latest/omdbpy.pdf 
 
   // Then run a request with axios to the OMDB API with the movie specified
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
 
   // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
+  // console.log(queryUrl);
 
-  axios.get(queryUrl).then(
-    function(response) {
+  axios
+    .get(queryUrl)
+    .then(function (response) {
+      //Response
+      // console.log(response.data);
       //Title of the movie.
-      console.log("Movie Title: "+ response.data.Title);
+      console.log("\t Movie Title: " + response.data.Title);
       // Year the movie came out.
-      console.log("Release Year: " + response.data.Year);
+      console.log("\t Release Year: " + response.data.Year);
       // IMDB Rating of the movie.
-      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("\t IMDB Rating: " + response.data.imdbRating);
       // Rotten Tomatoes Rating of the movie.
-      console.log("RT Rating: " + response.data.tomatoRating);
+      console.log("\t RT Rating: " + response.data.tomatoRating);
       // Country where the movie was produced.
-      console.log("Country Produced: " + response.data.Country);
+      console.log("\t Country Produced: " + response.data.Country);
       // Language of the movie.
-      console.log("Language: " + response.data.Language);
+      console.log("\t Language: " + response.data.Language);
       // Plot of the movie.
-      console.log("Plot: " + response.data.Plot);
+      console.log("\t Plot: " + response.data.Plot);
       // Actors in the movie.
-      console.log("Actors: " + response.data.Actors);
+      console.log("\t Actors: " + response.data.Actors);
+    })
+    .catch(function (error) {
+      if (error) {
+        console.log(error);
+      }
+    })
+};
+
+///////////////////////////////
+// Do What It Says
+///////////////////////////////
+
+function doitNow() {
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
     }
   
-  );
+    // We will then print the contents of data
+    console.log(data);
+  
+    // Then split it by commas
+    // var dataArr = data.split(",");
+  
+    // Then re-display the content as an array.
+    // console.log(dataArr);
+  
+  });
 
-}
+};
 
-
-// Do What It Says
-////////////////////////////////////////////////
-
-function doit() {
-
-    // Store all of the arguments in an array
-    var nodeArgs = process.argv;
-
-    // Create an empty variable for holding the band name
-    var search = "";
-
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "+"s
-    for (var i = 3; i < nodeArgs.length; i++) {
-
-    if (i > 3 && i < nodeArgs.length) {
-        search = search + "+" + nodeArgs[i];
-    }
-    else {
-        search += nodeArgs[i];
-    }
-
-    console.log(search);
-
-  }
-
-}
